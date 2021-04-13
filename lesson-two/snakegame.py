01a8bf5b3422a78f6803832583aa24c8e49b5e33
lilysunflower33 <>
2021-04-13 18:05:02 +0000

    snakegame.py basic snake

--- lesson-two/snakegame.py
+++ lesson-two/snakegame.py
@@ -12,7 +12,7 @@ def snake(stdscr):
   stdscr.nodelay(True)
   # set the timeout to milliseconds 
   # timeout is the defult moving speed for the snake 
-  stdscr.timeout(150)
+  stdscr.timeout(200)
   
   # draw a rectangle 
   textpad.rectangle(stdscr, 2, 2, sh - 2, sw - 2)
@@ -25,10 +25,10 @@ def snake(stdscr):
   ]
   snake_ch = chr(9608)
 
-  #9608 
+  # 9608 
   # paint the snake 
   for unit in snake:
-    stdscr.addstr(unit[0], unit[1], chr(9608))
+    stdscr.addstr(unit[0], unit[1], snake_ch)
 
   # set up the defult moving direction 
   direction = curses.KEY_RIGHT
@@ -36,8 +36,8 @@ def snake(stdscr):
   # randomly generate and place the snake food 
   food_ch = "*"
   food = [
-    random.randint(4, sh - 3)
-    random.randint(4, sw - 3)
+    random.randint(3, sh - 3),
+    random.randint(3, sw - 3)
   ]
   stdscr.addstr(food[0], food[1], food_ch)
 
@@ -48,19 +48,19 @@ def snake(stdscr):
     # exit game when player preses ESC, q, or Q
     if user_key in [27, 113, 81]:
       break 
-    #elif user_key in [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT]:
-    # move the snake unit one right 
-    # reset the initial direction 
-      #direction = user_key
 
-    if user_key == curses.KEY_UP: 
-      direction = user_key 
-    elif user_key == curses.KEY_LEFT: 
-      direction = user_key 
-    elif user_key == curses.KEY_RIGHT: 
-      direction = user_key 
-    elif user_key == curses.KEY_DOWN: 
-      direction = user_key 
+    if user_key in [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT]:
+    # reset the initial direction 
+      direction = user_key
+
+    #if user_key == curses.KEY_UP: 
+      #direction = user_key 
+    #elif user_key == curses.KEY_LEFT: 
+      #direction = user_key 
+    #elif user_key == curses.KEY_RIGHT: 
+      #direction = user_key 
+    #elif user_key == curses.KEY_DOWN: 
+      #direction = user_key 
 
     # add new head
     head = snake[0] 
@@ -76,13 +76,22 @@ def snake(stdscr):
       new_head = [head[0] + 1, [1]]
 
     # calculate the new snake after it moves
-    # new_head = [head[0], head[1] +1] *doesn't work*
     stdscr.addstr(new_head[0], new_head[1], snake_ch)
     # store the new head
     snake.insert(0, new_head)
-    # erase the tail by painting an white space
-    stdscr.addstr(snake[-1][0], snake[-1][1], ' ')
+
+    # check if the snake ate the food
+    if new_head != food:
+    # erase the tail by painting a white space
+      stdscr.addstr(snake[-1][0], snake[-1][1], ' ')
     snake.pop()
+  else: 
+    # place the new snake food randomly
+    food = [
+      random.randind(3, sh - 3),
+      random.randind(3, sw - 3)
+    ]
+    stdscr.addstr(food[0], food[1], food_ch)
 
   stdscr.getch() 
 
